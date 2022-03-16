f613c1149ab806ec7786488c499a022a58ee45fd
adir-jscode <adir.jscode@gmail.com>
2022-03-16 07:10:12 +0600

    JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Afsana Ara';
+const number = 600;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello React,lets Go</h3>
+        </div>
+        <div className="music">
+          <p>Name: {name}</p>
+          <p>Number : {10+ number }</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
